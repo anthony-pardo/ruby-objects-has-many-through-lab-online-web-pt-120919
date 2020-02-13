3c7ca8217544f0e46d48cb6bb6826e18be1895a2
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-13 21:11:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,7 +12,9 @@ class Artist
   end
   
   def new_song(name, genre)
-    song = Song.new(name, self, genre)
+    new_genre = Genre.new(genre)
+    song = Song.new(name, self, new_genre)
+    song
   end
     
   def self.all 
