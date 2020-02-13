183d3bf185ba561b56a95a1c01ce9fc6fabbdab6
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-13 22:28:53 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,5 +8,13 @@ class Patient
     @@all << self 
   end 
   
+  def new_appointment(doctor, date)
+    new_appointment = Appointment.new()
+  end
   
+  def self.all 
+    @@all.each do |patient|
+      puts patient.name 
+    end 
+  end
 end
