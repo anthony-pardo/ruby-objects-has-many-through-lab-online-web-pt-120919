f22a66ce3e4985fba87f5e1cbe7c1a3fff2a6066
Anthony Pardo <pardo.anthony@gmail.com>
2020-02-13 21:31:17 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,12 +3,13 @@ class Genre
   
   def initialize(name)
     @name = name 
+    @@all << self
   end
   
   @@all = []
   
   def songs 
-    
+    Song.all.select{|song| song.genre == self}
   end
   
   def self.all 
